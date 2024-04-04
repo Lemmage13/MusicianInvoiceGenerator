cd800b09f0ae2dce5e82a31de8595cece2386cb1
Matt <matthewwilliam.cole@gmail.com>
2024-04-04 12:11:14 +0100

    Added debug starter window to test DB connections once implemented

---
+++ MusicianInvoiceGenerator/Views/DebugWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MusicianInvoiceGenerator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MusicianInvoiceGenerator.Views
+{
+    /// <summary>
+    /// Interaction logic for DebugWindow.xaml
+    /// </summary>
+    public partial class DebugWindow : Window
+    {
+        public DebugWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel VM = new MainWindowViewModel();
+            MainWindow MW = new MainWindow();
+            MW.DataContext = VM;
+            MW.Show();
+        }
+    }
+}
