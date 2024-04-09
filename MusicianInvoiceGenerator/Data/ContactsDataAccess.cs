9f136552c46a534a66fee6f64395062bed26b03d
Matt <matthewwilliam.cole@gmail.com>
2024-04-09 12:14:40 +0100

    tweaked AddContact method to return Id of contact added + added scaffold method for finding contacts for use in DBRelay

--- MusicianInvoiceGenerator/Data/ContactsDataAccess.cs
+++ MusicianInvoiceGenerator/Data/ContactsDataAccess.cs
@@ -15,18 +15,32 @@ namespace MusicianInvoiceGenerator.Data
         {
 
         }
-        public void AddContact(ContactDetails c)
+        public int AddContact(ContactDetails c)
         {
-            string insertString = "INSERT INTO Contacts (Name, PhoneNumber, AddrsL1, AddrsL2, AddrsTown, AddrsPostCode) VALUES " +
+            int id;
+            string insertString = $"INSERT INTO {table} (Name, PhoneNumber, AddrsL1, AddrsL2, AddrsTown, AddrsPostCode) VALUES " +
                 $"('{c.Name}','{c.PhoneNumber}','{c.Line1}','{c.Line2}','{c.Town}','{c.Postcode}')";
+            string getIdString = $"SELECT TOP 1 Id FROM {table} ORDER BY Id Desc";
             Debug.WriteLine(insertString);
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand insertCommand = new SqlCommand(insertString, connection);
                 insertCommand.ExecuteNonQuery();
+                SqlCommand getIdCommand = new SqlCommand(getIdString, connection);
+                var idTemp = getIdCommand.ExecuteScalar();
+                if (idTemp == null) { throw new ArgumentException("idTemp cannot be null", nameof(idTemp)); }
+                id = Convert.ToInt32(idTemp);
                 connection.Close();
             }
+            return id;
+        }
+        public int? FindContact(ContactDetails c)
+        {
+            //Find entry based on name
+            //verify all other fields
+            //if all fields match return Id
+            return null; //TEMPORARY
         }
     }
 }
